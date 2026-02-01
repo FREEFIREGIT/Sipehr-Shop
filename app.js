bccd20ac04cf4a322e9912db63bca602c6765692
FREEFIRE <samirdv888@gmail.com>
2026-02-01 22:45:07 +0500

    Update app.js

--- app.js
+++ app.js
@@ -217,4 +217,18 @@ function hideSkeleton() {
   });
 }
 
+// ====== DARK MODE ======
+const darkToggle = document.getElementById("darkToggle");
+darkToggle.addEventListener("click", () => {
+  document.body.classList.toggle("dark-mode");
+  if(document.body.classList.contains("dark-mode")) {
+    localStorage.setItem("darkMode", "true");
+  } else {
+    localStorage.removeItem("darkMode");
+  }
+});
 
+// Включаем dark mode при загрузке
+if(localStorage.getItem("darkMode") === "true") {
+  document.body.classList.add("dark-mode");
+}  
